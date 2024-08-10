725d3e501616af767f9e450d13c6172fc729a5cd
Ajay2023 <ajayn1803@gmail.com>
2024-08-10 11:18:37 +0530

    Firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -15,7 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
 const provider = new GoogleAuthProvider();
 export { auth, provider };
